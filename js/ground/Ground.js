aeafd5486df76ac458076f1ac01b2ae6213d224c
DESKTOP-1CC1P0H\hanhi <hieuth160597@gmail.com>
2017-08-13 12:56:03 +0700

    Merge branch 'master' of C:\Users\hanhi\OneDrive\Documents\EchKeuTroiMua with conflicts.

--- js/ground/Ground.js
+++ js/ground/Ground.js
@@ -1,6 +1,6 @@
 class Ground {
     constructor(x, y, spritename, config) {
-        this.sprite = Nakama.groundGroup.create(x, y, 'planets', spritename);
+        this.sprite = Nakama.groundGroup.create(x, y, spritename);
         this.config = config;
         this.sprite.update = this.update.bind(this);
         this.sprite.width = 640;
